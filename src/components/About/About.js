5012c20dd113df1584e9da104a2ac8d2208ba313
nohaderf <freda.hon@gmail.com>
2021-07-20 23:11:44 -0400

    edited about and contacts section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,9 +15,13 @@ function About() {
             <div className="content-div">
                 <h1>about</h1>
                 <div>
-                    <p>I'm a fullstack developer based in Toronto, Canada.</p>
-                        <span>I have a creative eye with a passion for web design. 
-                            Many of my personal projects feature small cameos from my Photoshopping hobby. 
+                    <p>I'm a full stack developer, based in Toronto, who left medicine to pursue a creative path in software development.
+                        While it is an uncommon career change, web design has always been a passion of mine, 
+                        and I believe that software development can bring more change and impact in the world 
+                        than my previous role in medicine. </p>
+                        <p>I am well versed in JavaScript, ReactJS, HTML, CSS, postgreSQL, Python and Ruby on Rails. 
+                            I possess strong skills in Adobe Photoshop and have experience with converting images to HTML/CSS layouts. Additionally, I am adept in component performance optimization, scalable software design, designing and translating wireframes, and building reusable cross platform components.</p>
+                        <span>
                             When I'm not on the computer, you can find me experimenting on canvases,
                             hiking some scenic routes, or cuddled up in a corner with a good book. 
                         </span>
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,18 +1,15 @@
+import contactImage from './Images/portfolio-contact.png';
+import contactImageC from './Images/portfolio-contact-cyan.png';
+import contactImageO from './Images/portfolio-contact-orange.png';
+import contactImageB from './Images/portfolio-contact-blue.png';
+
+
 function Contact() {
     return (
-        <div className="container"  id="contact">
-            <h1>contact</h1>
+        <div  className="container" id="contact">
+            <h1>contact me</h1>
             <div className="container-div">
-                <div>
-                <p>Feel free to reach out or connect with me!</p>
-                <p className="text">
-                <span>E-mail <i class="fas fa-envelope"></i>:</span> freda.hon@gmail.com</p>
-                <p className="text">
-                <span>LinkedIn <i className="fab fa-linkedin"></i>:</span> <a href="https://www.linkedin.com/in/freda-hon/">freda-hon</a></p>
-                <p className="text">
-                <span>Medium <i className="fab fa-medium"></i>:</span> <a href="https://medium.com/@freda.hon">@freda.hon</a></p>
-            </div>
-            <div className="container-form">
+                <div className="container-form">
                 {/* <h2>Inquiry Form</h2> */}
                 <form 
                     action="mailto:freda.hon@gmail.com"
@@ -22,11 +19,29 @@ function Contact() {
                 >
                     Name: <br></br><input type="text"></input><br></br>
                     Email: <br></br> <input type="email" name="Contact-Email"></input><br></br>
-                    Message: <textarea name="Contact-Message" rows="6" cols="50"> </textarea><br></br>           
+                    Message: <br></br><textarea name="Contact-Message" rows="6" cols="50"> </textarea><br></br>           
                     <button type="submit" value="Submit">Send</button>
                 </form> 
             </div>
+
+            <div className="contacts-div">
+                <div>
+                <p>Feel free to reach out or connect with me!</p>
+                <p className="text">
+                <span>E-mail <i class="fas fa-envelope"></i>:</span> freda.hon@gmail.com</p>
+                <p className="text">
+                <span>LinkedIn <i className="fab fa-linkedin"></i>:</span> <a href="https://www.linkedin.com/in/freda-hon/">freda-hon</a></p>
+                <p className="text">
+                <span>Medium <i className="fab fa-medium"></i>:</span> <a href="https://medium.com/@freda.hon">@freda.hon</a></p>
+            </div>
             
+            <div className="contact-imgs">
+                <img src={contactImage} alt="Freda" className="contact-img"></img>
+                <img src={contactImageO} alt="Freda" className="contact-img-o"></img>
+                <img src={contactImageB} alt="Freda" className="contact-img-b"></img>
+                <img src={contactImageC} alt="Freda" className="contact-img-c"></img>
+            </div>
+            </div>
 
         </div>
       </div>
