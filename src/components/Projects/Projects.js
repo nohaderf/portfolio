620fce43efdd55d653851a9a7d46027dc1acbe4f
nohaderf <freda.hon@gmail.com>
2021-07-22 23:56:26 -0400

    loaded projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,6 +2,25 @@ export default function Projects() {
     return (
         <div className="container" id="projects">
           <h1>projects</h1>
+          <div className="projects-div">
+
+            <div className="project">
+              <h2>Life League</h2>
+              <img src="https://i.imgur.com/927YK49.png" alt="life-league" className="project-thumbnail"/>
+              <div className="project-desc">A competitive goal management app where users challenge their friends to complete goals and hold each other accountable.</div>
+            </div>
+
+            <div className="project">
+              <h2>Krunch</h2>
+              <a href="https://krunch.netlify.app/" target="_blank" rel="noopener noreferrer">
+                <img src="https://d33wubrfki0l68.cloudfront.net/602f02201639380008f0359f/screenshot.png" alt="life-league"className="project-thumbnail"/>
+              </a>
+              <div className="project-desc">An app that provides a vast exercise-guide library and allow users to plan their workouts and track their progress.</div>
+            </div>
+        </div>
+
+
+          
         </div>
     )
 }
