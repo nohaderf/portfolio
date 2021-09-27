c58525bea2dacf381de45c45c8b90267f79fd728
nohaderf <freda.hon@gmail.com>
2021-09-27 01:33:53 -0400

    added user data project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -17,8 +17,15 @@ export default function Projects() {
               </a>
               <div className="project-desc">An app that provides a vast exercise-guide library and allow users to plan their workouts and track their progress.</div>
             </div>
+            
+            <div className="project">
+              <h2>User Data</h2>
+              <a href="https://userdatacc.netlify.app/" target="_blank" rel="noopener noreferrer">
+                <img src="https://i.imgur.com/tFmfI2m.png" alt="life-league" className="project-thumbnail"/>
+              </a>
+              <div className="project-desc">Simple application that displays user data in table and modal format.</div>
+            </div>
         </div>
-
 
           
         </div>
